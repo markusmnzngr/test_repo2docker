8e081e12c2e17eb8c3b2635d109f645faf4e425c
Markus Münzinger <m.muenzinger@ioer.de>
2025-04-22 15:58:34 +0200

    Create install.R

---
+++ install.R
@@ -0,0 +1,11 @@
+install.packages("IRkernel")
+IRkernel::installspec(user = FALSE)
+
+# Add other package installations as needed
+install.packages("data.table")
+install.packages("ggplot2")
+install.packages("here")
+install.packages("leaflet")
+install.packages("patchwork")
+install.packages("sf")
+install.packages("terra")
